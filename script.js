9fa087cecb521951f605a0de3baff0a1660215f7
alessandro <ale_hagy@hotmail.com>
2023-10-11 19:31:48 -0300

    Ajuste codigo

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ h2.style.fontSize = "40px"
 
 //insere cor no campo de login devido a nao informar login
 const username = document.querySelector('#login-usuario')
-username.classList.add('error')
+username.classList.add('correct')
 
 //torna mensagem de erro visivel associado ao campo login
-const errorMessage = document.querySelector ('.error-text')
-errorMessage.classList.add('visible')
+const errorMessage = document.querySelectorAll ('.error-text')
+errorMessage[1].classList.add('visible')
 
 //insere cor no campo de senha devido a nao informar senha
 const senha = document.querySelector('#login-senha')
 senha.classList.add('error')
 
 //torna mensagem de erro visivel associado ao campo senha
-//const errorMessage = document.querySelector ('.error-text')
-//errorMessage.classList.add('visible')
+//trocou a linha 8 do codigo para correct antes estava error
+//trocou  a linha 11 de querySelect para querySelectAll
+//como esta usando querySelectAll, ele vai pegar todos
+//trocou a linha 12 de errorMessage para errorMessage[1]
 
 //coloca cor na fonte do botao
 const botao = document.querySelector('button') 
